7c0096fd52cbb09acc80f4481969d4245eafcc67
Benjamin Goering <bengoering@gmail.com>
2014-06-25 15:21:28 -0700

    use stream-client via Livefyre.require

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var ActivityList = require('activity-list');
 var ChronosStream = require('chronos-stream');
+
 // will be provided by Livefyre.js later
 var auth;
 
@@ -7,6 +8,10 @@ console.log('loading personalized feed example');
 
 var feed = new ActivityList(document.getElementById('feed'));
 feed.streamMore(require('./mock-activity-stream'));
+// feed.streamMore(chronosActivityStream(
+//     'urn:livefyre:profiles-qa.fyre.co:user=5329c8c285889e7bc6000000:personalStream',
+//     auth))
+
 
 // create an authenticated stream of activities
 // from chronos
@@ -41,4 +46,29 @@ Livefyre.require(['auth-contrib#0.0.0-pre'], function (authContrib) {
     auth.delegate(auth.createDelegate('http://qa-ext.livefyre.com'));
     var authLog = authContrib.createLog(auth, document.getElementById('auth-log'));
     authContrib.createButton(auth, document.getElementById('auth-button'), authLog);
+});
+
+/**
+ * Get streamclient 0.0.0 from the cdn
+ */
+Livefyre.require([
+  'stream-client#0.0.3-dev.1'
+], function (StreamClient) {
+    var auth = Livefyre.auth;
+  // Connect to stream
+  var sc = new StreamClient({
+    hostname: "stream.qa-ext.livefyre.com", port: "80"});
+  
+  var user = auth.get('livefyre');
+  if (user) {
+    onLogin(user);
+  } else {
+    auth.on('login.livefyre', onLogin);
+  }
+  function onLogin(user) {
+    var token = user.get('token');
+    sc.connect(token, "sample");
+    feed.stream(sc); 
+  }
+  
 });
